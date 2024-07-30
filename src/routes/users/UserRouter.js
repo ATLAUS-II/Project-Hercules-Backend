9ac90c762cfc4dba0c637ecb02549c44dec5ab51
axelaquino3 <axelaquino@google.com>
2024-07-30 16:26:45 -0400

    Deleted unnecessary routes from UserRouter.js

--- src/routes/users/UserRouter.js
+++ src/routes/users/UserRouter.js
@@ -3,17 +3,6 @@ const { User } = require('../../../models')
 
 const router = express.Router()
 
-// [ADMIN] POST Create a new User.
-router.post('/', async (req, res, next) => {
-  const { nickname, email, userId } = req.body
-  try {
-    const user = await User.create({ nickname, email, userId })
-    res.status(201).send({ user })
-  } catch (error) {
-    next(error)
-  }
-})
-
 // GET User by ID and if no user is found, create user.
 router.get('/', async (req, res, next) => {
   const userInfo = JSON.parse(req.headers['x-user-info'])
@@ -37,35 +26,5 @@ router.get('/', async (req, res, next) => {
   } 
 })
 
-router.get('/users', async (req, res, next) => {
-  try {
-    const users = await User.find()
-    console.log(users)
-    res.send(users)
-  } catch(error) {
-    res.statusCode(500).send({ error })
-  }
-})
-
-router.get("/users/:id", async (req, res, next) => {
-  try {
-    const id = req.params.id
-    const user = await User.findById(id)
-    res.send(user)
-  } catch (error) {
-    res.statusCode(404).send({ error })
-  }
-})
-
-router.post("/users", async (req, res, next) => {
-  const user = new User(req.body);
-
-  try {
-    console.log("Received Data, ", req.body)
-    await user.save({ new: true })
-  } catch (error) {
-    res.statusCode(500).send(error) 
-  }
-})
 
 module.exports = router
