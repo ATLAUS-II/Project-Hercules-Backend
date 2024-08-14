8e0553de833f3e5a2c70ef725a0eb7f1818169b5
Aaron Sanchez <saarong12@gmail.com>
2024-08-13 20:42:17 -0500

    Commented out the jwtCheck for testing.

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ const app = express()
 
 app.use(cors())
 // app.use(morgan('dev'))
-app.use(jwtCheck)
+// app.use(jwtCheck)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
